e0b68a753c5d2ae08919975602d3076d63d1b376
seigo.kawamoto <seigorilla0422@gmail.com>
2020-08-16 10:49:13 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -43,6 +43,12 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("tasklist", t);
         request.setAttribute("_token", request.getSession().getId());
 
+        // メッセージデータが存在しているときのみ
+        // メッセージIDをセッションスコープに登録
+        if(t != null) {
+            request.getSession().setAttribute("tasklist_id", t.getId());
+        }
+
         // メッセージIDをセッションスコープに登録
         request.getSession().setAttribute("tasklist_id", t.getId());
 
